3dc42d096e3c189c5b0da7af0330c82047e72db0
Ihor Onyshchuk <ihor-onyshchuk@MacBook-Pro.local>
2023-07-03 14:56:41 +0300

    Add authorization

--- index.js
+++ index.js
@@ -64,6 +64,49 @@ app.post('/auth/register', registerValidation, async (req, res) => {
   }
 });
 
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPassword) {
+      return res.status(400).json({
+        message: 'Check your login or password',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret-token-key',
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Authorization failed!');
+  }
+});
+
 app.listen(8080, (err) => {
   if (err) {
     return console.log(err);
