9f0f97424ade417c8ae2219e69bf1022434e1d61
Ihor Onyshchuk <ihor-onyshchuk@MacBook-Pro.local>
2023-07-03 13:36:04 +0300

    Add jwt token

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import mongoose from "mongoose";
-import { validationResult } from "express-validator";
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
 import 'dotenv/config';
 
-import UserModel from "./models/User.js";
-import { registerValidation } from "./validations/auth.js";
+import UserModel from './models/User.js';
+import { registerValidation } from './validations/auth.js';
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -21,29 +22,46 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      passwordHash: hash,
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret-token-key',
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Ooops! Registration failed!');
   }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    passwordHash,
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-  });
-
-  const user = await doc.save();
-
-  res.json({
-    user,
-    success: true,
-  })
 });
 
 app.listen(8080, (err) => {
